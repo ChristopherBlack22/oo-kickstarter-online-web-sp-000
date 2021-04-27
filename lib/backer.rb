960fc1b86327bfc9cf9eb8eb7580167d7a470553
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-27 22:08:49 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,10 @@
 class Backer
-  attr_accessor :name 
+  attr_accessor :name, :back_projects 
   
-  @backed_project = [] 
   
   def initialize(name)
     @name = name
-    
+    @backed_projects = []
   end 
   
 end 
